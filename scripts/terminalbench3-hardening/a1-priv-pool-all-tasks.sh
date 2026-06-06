5044f128ab5c4b852102f7012990c641eaef208d
Ivgeni Segal <ivgeni.segal@gmail.com>
2026-06-06 00:23:46 +0000

    Updated the all-tasks script to start privileged hack at 3rd iteration.

--- scripts/terminalbench3-hardening/a1-priv-pool-all-tasks.sh
+++ scripts/terminalbench3-hardening/a1-priv-pool-all-tasks.sh
@@ -2,12 +2,13 @@ BOOTSTRAP_TASK=binary-sprite-extraction
 cd ~/truthserum/harden-v0 \
 && uv run python harden.py --tasks-dir ../datasets/terminalbench3_review2/_flattened \
 --all \
---solver-model gemini/gemini-3-flash-preview \
---hacker-model gemini/gemini-3-flash-preview \
---fixer-model gemini/gemini-3-flash-preview \
+--oracle \
+--solver-model gemini/gemini-3.5-flash \
+--hacker-model gemini/gemini-3.5-flash \
+--fixer-model gemini/gemini-3.5-flash \
 --solver-privileged \
 --hacker-privileged \
---hacker-privileged-disable-iteration 8 \
+--hacker-privileged-enable-iteration 3 \
 --max-concurrent 100 \
 --solver-max-turns 200 \
 --hacker-max-turns 200 \
@@ -20,4 +21,4 @@ cd ~/truthserum/harden-v0 \
 --pool-bootstrap-dir=../datasets/terminalbench3_review2/_flattened/$BOOTSTRAP_TASK/ \
 --fixer-prompt-file=./prompts/fixer_guidance.md \
 --fixer-prompt-after-iter 5 \
---output-dir outputs/terminalbench3_review2_latestharbor_alltasks \
+--output-dir outputs/terminalbench3_review2_start-priv3 \
